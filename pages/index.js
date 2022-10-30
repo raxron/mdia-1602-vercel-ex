2863a9c6cd14d3ab7f6692a6ad35d0b1989cee74
RachelX <xia.rong.rachel@gmail.com>
2022-10-29 18:21:31 -0700

    update

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          What does C.R.U.D. stand for?
           <code className={styles.code}>pages/index.js</code>
         </p>
 
